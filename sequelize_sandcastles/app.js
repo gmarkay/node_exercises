5c3859ba436af6a5f5eae9a6021d4e385bdd1ecb
gmarkay <gmarkay@gmail.com>
2018-04-14 18:24:41 -0500

    number 5 sequelize seeding

--- sequelize_sandcastles/app.js
+++ sequelize_sandcastles/app.js
@@ -5,10 +5,63 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set("models", require('./models'))
+const { Beach, Lifeguard, Shark, SharkType } = app.get("models");
 
-const { Beach, Lifeguard } = app.get('models');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// app.get('/beaches', (req, res, next)=>{
-//   Beach.findAll
-// })
+app.get('/beaches', (req, res, next) => {
+  Beach.findAll({ include: [{ model: Lifeguard, attributes: ["name"], model: Shark, attributes: ["name", "eats_humans"] }] })
+    .then((beaches) => {
+      res.status(200).json(beaches);
+    });
+});
+app.get('/beaches/:id', (req, res, next) => {
+  Beach.findOne({
+    raw: true,
+    include: [{ model: Lifeguard, attributes: ["name"], model: Shark, attributes: ["name", "eats_humans"] }],
+    where: { id: req.params.id },
+  })
+    .then((beach) => {
+      res.status(200).json(beach);
+    });
+});
+
+app.get('/lifeguards', (req, res, next) => {
+  Lifeguard.findAll({ include: { model: Beach, attributes: ["name", "location"] } })
+    .then((lifeguards) => {
+      res.status(200).json(lifeguards);
+    })
+})
+app.get('/lifeguards/:id', (req, res, next) => {
+  Lifeguard.findOne({
+    raw: true,
+    where: { id: req.params.id },
+    include: { model: Beach, attributes: ["name", "location"] }
+  })
+    .then((lifeguard) => {
+      res.status(200).json(lifeguard);
+    });
+});
+
+app.post('/beaches', ({ body: { name, location, sand_rating } }, res, next) => {
+  Beach.create({name, location,sand_rating })
+    .then((newBeach) => {
+      console.log(newBeach.dataValues)
+      res.status(201).json(newBeach);
+    });
+});
+app.post('/lifeguards', ({ body: { first_name, last_name, beachId } }, res, next) => {
+  Lifeguard.create({ first_name, last_name, beachId})
+    .then((newLG) => {
+      console.log(newLG.dataValues)
+      res.status(201).json(newLG);
+    });
+});
+
 module.exports = app;
+
+const port = process.env.PORT || 5555;
+app.listen(port, () => {
+  console.log(`listen on port ${port}`);
+});
--- sequelize_sandcastles/build-db.js
+++ sequelize_sandcastles/build-db.js
@@ -1,13 +1,28 @@
 let sequelize = require('sequelize');
 let queryInterface = require('sequelize/lib/query-interface');
 
-((queryInterface) =>{
+const { beaches } = require('./seeders/data/beaches.json');
+const { lifeguards } = require('./seeders/data/lifeguards.json');
+const { sharkTypes } = require('./seeders/data/sharkTypes.json');
+const { sharks } = require('./seeders/data/sharks.json');
+
+
+((queryInterface) => {
   const app = require('./app'); //make sure you are exporting app after attaching the necessary pieces in app.js!
   const models = app.get('models');
-  return models.sequelize.sync({force: true})
-  .then((queryInterface)=>{
-  })
-  .catch((err)=>{
-  console.log('this shouldnt happen');
-  })
+  return models.sequelize.sync({ force: true })
+    .then((queryInterface) => {
+      return models.Beach.bulkCreate(beaches);
+    })
+    .then((queryInterface)=>{
+      return models.Lifeguard.bulkCreate(lifeguards);
+    })
+    .then((queryInterface)=>{
+      return models.SharkType.bulkCreate(sharkTypes);
+    }).then((queryInterface)=>{
+      return models.Shark.bulkCreate(sharks);
+    })
+    .catch((err) => {
+      console.log(err, 'this shouldnt happen');
+    })
 })();
